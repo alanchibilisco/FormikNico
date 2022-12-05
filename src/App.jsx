ab4224b02543263e6804b8eb320d14214b5d3537
nicolas <nicolasviruel@gmail.com>
2022-12-05 18:53:55 -0300

    Se agrego la carpeta de routes para tener un mejor manejo de las rutas y ademas se comenzo con el maquetado del footer, faltaria importar font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./components/pages/Home"
-import Login from "./components/pages/Login"
+import RouterApp from "../routes/RouterApp"
 import Footer from "./components/layout/Footer"
-import TablaProducto from "./components/pages/TablaProducto"
 import NavBar from "./components/layout/Navbar"
-import CreacionProducto from "./components/pages/CreacionProducto"
-import EdicionProducto from "./components/pages/EdicionProducto"
-import Error404 from "./components/pages/Error404"
-import Nosotros from "./components/pages/Nosotros"
+
 
 
 const App = () => {
@@ -18,16 +13,8 @@ const App = () => {
         <NavBar/>
         
         <main>
-          <Routes>
-            <Route exact path="/" element={<Home  />} />
-            <Route exact path="/login" element={<Login  />} />
-            <Route exact path="/tablaproducto" element={<TablaProducto  />}/>
-            <Route exact path="/creacionproducto" element={<CreacionProducto  />}/>
-            <Route exact path="/edicionproducto" element={<EdicionProducto />}/>
-            <Route exact path="/sobrenosotros" element={<Nosotros />}/>
-            <Route exact path="*" element={<Error404/>}/>
-          </Routes>
-          </main>  
+          <RouterApp/>
+        </main>  
       </BrowserRouter>
 
     <Footer/>
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+  <>
+    <Container className="container-fluid">
+            <Row className="row bg-dark align-items-center">
+                <Col className="col-md-4 col-sm-12 text-center">
+                    {/* <img src="/Imagenes/Imagen1.png" alt="logo" width="200"> */}
+                </Col>
+
+                <Col className="col-md-4 col-sm-12 text-center">
+                    <h5 className="mb-3 text-white font-weight-bolder mt-3">Nuestras redes Sociales</h5>
+                    <a href="https://www.facebook.com/login/"
+                        className="fa-brands fa-facebook redes text-center text-decoration-none mx-2 mt-2"
+                        target="_blank"></a>
+                    <a href="https://www.whatsapp.com/"
+                        className="fa-brands fa-whatsapp redes text-center text-decoration-none mx-2 mt-2"
+                        target="_blank"></a>
+                    <a href="https://twitter.com/i/flow/login"
+                        className="fa-brands fa-twitter redes text-center text-decoration-none mx-2 mt-2"
+                        target="_blank"></a>
+                    <a href="tel:+549-3813368181"
+                        className="fa-solid fa-phone redes text-center text-decoration-none mx-2 mt-2" target="_blank"></a>
+                    <a href="https://www.youtube.com/"
+                        className="fa-brands fa-youtube redes text-center text-decoration-none mx-2 mt-2"
+                        target="_blank"></a>
+                    <a href="https://www.instagram.com/accounts/login/?"
+                        className="fa-brands fa-instagram redes text-center text-decoration-none mx-2 mt-2"
+                        target="_blank"></a>
+                    <p className="mt-4 text-white font-weight-bolder border-bottom-2">Copyrigth 2022 © TODOS LOS DERECHOS
+                        RESERVADOS <span className="text-info">EQUIPO Nº4</span> </p>
+                </Col>
+
+                <Col className="col-md-4 col-sm-12">
+                    <p className="text-white text-center mt-2 mb-2">Los precios cotizados pueden variar sin previo aviso y no
+                        incluyen IVA - Factura A.
+                        Todas las imágenes y descripciones comerciales no son contractuales. Las descripciones de todos
+                        los productos y
+                        condiciones se encuentran sujetas a modificaciones sin previo aviso, pueden contener errores o
+                        pueden no contener
+                        todos los datos descriptivos. A los fines de obtener un asesoramiento completo e integral
+                        deberán concurrir al
+                        local.
+                    </p>
+
+                    <Col className="text-center">
+                        <a href="/404.html" className="text-center">Terminos y Condiciones</a>
+                    </Col>
+                </Col>
+            </Row>
+        </Container>
+  
+  
+  </>
+    
+   
   )
 }
 
