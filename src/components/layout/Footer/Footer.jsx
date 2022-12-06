21e673e0594f0ce5441dc70055ebe643f14a5e02
nicolas <nicolasviruel@gmail.com>
2022-12-05 22:12:48 -0300

    Corregimos errores finales para comenzar a trabajar

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import logo from "../../../imagenes/Imagen1.png";
 //importamos font
+import "../../../../css/Footer.css"
 
 
 
@@ -11,15 +12,15 @@ const Footer = () => {
   <>
     <Container className="container-fluid">
             <Row className="row bg-dark align-items-center">
-                <Col className="col-md-4 col-sm-12 text-center">
+                <Col className=" text-center" xs={12} md={4}>
                     <img src={logo} alt="logo" width="200"/>
                 </Col>
 
-                <Col className="col-md-4 col-sm-12 text-center">
+                <Col className=" text-center" xs={12} md={4}>
                     <h5 className="mb-3 text-white font-weight-bolder mt-3">Nuestras redes Sociales</h5>
                     <a href="https://www.facebook.com/login/"
-                        className="  text-center border border-danger mx-2 mt-2"
-                        target="_blank">asdasdasd</a>
+                        className=" redes fa-brands fa-facebook text-decoration-none text-center mx-2 mt-2"
+                        target="_blank"></a>
                     <a href="https://www.whatsapp.com/"
                         className="fa-brands fa-whatsapp redes text-center text-decoration-none mx-2 mt-2"
                         target="_blank"></a>
@@ -38,7 +39,7 @@ const Footer = () => {
                         RESERVADOS <span className="text-info">EQUIPO Nº4</span> </p>
                 </Col>
 
-                <Col className="col-md-4 col-sm-12">
+                <Col className="" xs={12} md={4}>
                     <p className="text-white text-center mt-2 mb-2">Los precios cotizados pueden variar sin previo aviso y no
                         incluyen IVA - Factura A.
                         Todas las imágenes y descripciones comerciales no son contractuales. Las descripciones de todos
