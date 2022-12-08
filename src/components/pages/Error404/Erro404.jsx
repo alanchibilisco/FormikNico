a5fd495634f75df172b6ec346db869c6d58c4313
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2022-12-08 16:56:41 -0300

    se creo la pagina error404

--- src/components/pages/Error404/Erro404.jsx
+++ src/components/pages/Error404/Erro404.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Container, Image, Button} from 'react-bootstrap'
+import error404 from '../../../assets/img/error404.gif'
 
 const Erro404 = () => {
   return (
-    <div>Erro404</div>
+    <Container className='text-center mb-5'>
+      <Image src={error404}/>
+      <div>
+        <h3>It seems there was an error 🤔 back to home!</h3>
+        <Button size="lg" variant="warning" className='my-4 text-decoration-none'><Link to="/" className=" text-decoration-none text-center text-black">Home</Link></Button>
+      </div>
+    </Container>
   )
 }
 
