60c3aa1286bb0958ed7ca5b13fd1b2720a0e235e
franco monteros <franco.n.monteros@gmail.com>
2022-12-09 14:18:29 -0300

    se agrego la pagina recuperar contraseña

--- src/components/pages/RecPassword/RecPassword.jsx
+++ src/components/pages/RecPassword/RecPassword.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Col, Container, Row,Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+
 
 const RecPassword = () => {
+  const navigate = useNavigate()
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'You will receive an email to your email to make the changes',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    navigate("/")
+
+  }
   return (
-    <div>RecPassword</div>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">enter Email</Form.Label>
+                <Form.Control id="disabledTextInput" placeholder="email" />
+              </Form.Group>
+              <button className='btn btn-warning btn-block'>send</button>
+            </Form>
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
