dccdf4934890fb65f86410e6d5a0fd77a736cf89
franco monteros <franco.n.monteros@gmail.com>
2022-12-08 22:39:45 -0300

    se modifico el footer y se agrego el qr fiscal

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -12,13 +12,41 @@ const Footer = () => {
 
         <Container fluid>
             <Row className="row bg-dark align-items-center">
-                <Col className=" text-center" xs={12} md={4}>
+                <Col className=" text-center" xs={12} md={6} lg={4}>
                     <img src={logo} alt="logo" width="200" />
                 </Col>
-
-                <Col className=" text-center" xs={12} md={4}>
-                    <h5 className="mb-3 text-white font-weight-bolder mt-3">Nuestras redes Sociales</h5>
-                    <a href="https://www.facebook.com/login/"
+                {/* <Col className=" text-center" xs={12} md={6} lg={4}>
+                    <img src="/src/assets/dataFiscal.png" alt="logo" width="50" />
+                </Col> */}
+                <Col className=" text-center" xs={12} md={6} lg={4}>
+                    {/* <h5 className="mb-3 text-white font-weight-bolder mt-3">Nuestras redes Sociales</h5> */}
+                    <div className='"d-flex justify-content-between"'>
+                        <div>
+                            <h5 className="mb-3 text-white font-weight-bolder mt-3">Nuestras redes Sociales</h5>
+                            <a href="https://www.facebook.com/login/"
+                                className=" redes fa-brands fa-facebook text-decoration-none text-center mx-2 mt-2"
+                                target="_blank"></a>
+                            <a href="https://www.whatsapp.com/"
+                                className="fa-brands fa-whatsapp redes text-center text-decoration-none mx-2 mt-2"
+                                target="_blank"></a>
+                            <a href="https://twitter.com/i/flow/login"
+                                className="fa-brands fa-twitter redes text-center text-decoration-none mx-2 mt-2"
+                                target="_blank"></a>
+                            
+                        </div>
+                        <div>
+                            <a href="tel:+549-3813368181"
+                                className="fa-solid fa-phone redes text-center text-decoration-none mx-2 mt-2" target="_blank"></a>
+                            <a href="https://www.youtube.com/"
+                                className="fa-brands fa-youtube redes text-center text-decoration-none mx-2 mt-2"
+                                target="_blank"></a>
+                            <a href="https://www.instagram.com/accounts/login/?"
+                                className="fa-brands fa-instagram redes text-center text-decoration-none mx-2 mt-2"
+                                target="_blank"></a>
+                        </div>
+                        <div className='mt-3'><img src="/src/assets/dataFiscal.png" alt="logo" width="50" /></div>
+                    </div>
+                    {/* <a href="https://www.facebook.com/login/"
                         className=" redes fa-brands fa-facebook text-decoration-none text-center mx-2 mt-2"
                         target="_blank"></a>
                     <a href="https://www.whatsapp.com/"
@@ -34,12 +62,12 @@ const Footer = () => {
                         target="_blank"></a>
                     <a href="https://www.instagram.com/accounts/login/?"
                         className="fa-brands fa-instagram redes text-center text-decoration-none mx-2 mt-2"
-                        target="_blank"></a>
+                        target="_blank"></a> */}
+                    {/* <img src="/src/assets/dataFiscal.png" alt="logo" width="50" /> */}
                     <p className="mt-4 text-white font-weight-bolder border-bottom-2">Copyrigth 2022 © TODOS LOS DERECHOS
                         RESERVADOS <span className="text-info">Las Fuerzas Especiales Ginyu</span> </p>
                 </Col>
-
-                <Col className="" xs={12} md={4}>
+                <Col className="text-center" xs={12} md={6} lg={4}>
                     <p className="text-white text-center mt-2 mb-2">Los precios cotizados pueden variar sin previo aviso y no
                         incluyen IVA - Factura A.
                         Todas las imágenes y descripciones comerciales no son contractuales. Las descripciones de todos
