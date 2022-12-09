89bcfcbb3e32ea61471849b591d52ab3c3274656
nicolas <nicolasviruel@gmail.com>
2022-12-09 09:19:11 -0300

    Se agrego la pagina de login, con css y imagen. Falta retocar los modals y dar funcionalidad

--- src/routes/RouterPrincipal.jsx
+++ src/routes/RouterPrincipal.jsx
@@ -13,6 +13,7 @@ import Destacados from "../components/pages/Destacados";
 import Favorito from "../components/pages/Favorito";
 import Footer from "../components/layout/Footer";
 import Headers from "../components/layout/Headers";
+import Login from "../components/pages/Login/Login";
 
 
 const RouterPrincipal = () => {
@@ -22,6 +23,7 @@ const RouterPrincipal = () => {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/recpassword" element={<RecPassword />} />
         <Route exact path="/tablaproducto" element={<TablaProducto />} />
         <Route exact path="/creacionproducto" element={<CreacionProducto />} />
