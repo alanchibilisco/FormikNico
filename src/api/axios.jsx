7b20e1a16ee17bfad252781bfe61221e0959ee02
josetrivino89 <josetrivino89@gmail.com>
2022-12-10 16:17:44 -0300

    se corrgio coneccion con bakc y se trajo los datos de las base de datos

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
+
 const url = import.meta.env.VITE_URL_BACKEND
 const instance = axios.create({
   baseURL: url ? url : "http://localhost:3000/api",
-  timeout: 1000,
+  timeout: 4000,
   headers: {'Content-Type':'application/json'}
 });
 
--- src/components/pages/TablaProducto/Producto/TablaProducto/TablaProductos.jsx
+++ src/components/pages/TablaProducto/Producto/TablaProducto/TablaProductos.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Container, Table, Button, Row, Col, Form } from "react-bootstrap";
+import { Container, Table, Button, Row, Col, Form, Spinner, Modal, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import 'boxicons';
 import instance from "../../../../../api/axios";
+
 
 const TablaProducto = () => {
 
@@ -11,7 +12,9 @@ const TablaProducto = () => {
   const getProductos =async()=>{
 
     try {
+      
       const resp = await instance.get("/productos/")
+      console.log(resp)
       setProducto(resp.data)
       
     } catch (error) {
@@ -19,6 +22,8 @@ const TablaProducto = () => {
       alert("Error")     
     }
   }
+
+
   useEffect(()=>{
     getProductos()
   },[])
@@ -66,41 +71,46 @@ const TablaProducto = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>IPA</td>
-                  <td>Cerveza Amarga</td>
-                  <td>$250</td>
-                  <td>
-                    <p className="truncate-img-link m-0">
-                      https://media.istockphoto.com/photos/fresh-croissants-on-wooden-board-and-orange-jam-in-bowl-picture-id1199388833?k=20&m=1199388833&s=612x612&w=0&h=RzQ3L1uDy9Id4Xx5-GxMeGvm5V00J02F_nnksyZ0f-s=
-                    </p>
-                  </td>
-                  <td>Cerveza</td>
-                  <td>3%</td>
-                  <td>si</td>
-                  <td className="w-25">
-                    <div className="d-flex justify-content-center">
-                      <Button variant="outline-success mx-1">
-                        <Link
-                          to="/productpage/edicionproducto"
-                          variant="outline-primary mx-1"
-                        >
-                          <box-icon
-                            type="solid"
-                            name="message-square-edit"
-                          ></box-icon>
-                        </Link>
-                      </Button>
-                      <Button variant="outline-danger mx-1">
-                        <box-icon
-                          name="message-square-x"
-                          type="solid"
-                        ></box-icon>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
+    {
+
+      producto.length > 0 ? producto.map( ( prod, index) => (
+        <tr key={prod._id} >
+        <td>{index}</td>
+        <td>{prod.ProductName}</td>
+        <td>{prod.Productdetalle}</td>
+        <td>${prod.PriceProduct}</td>
+        <td>
+          <Image className="truncate-img-link m-0" src={prod.ImgURL} fluid/>
+        </td>
+        <td>{prod.Category}</td>
+        <td>{prod.Graduation}</td>
+        <td>{prod.Avaliable ? 'si' : 'no'}</td>
+        <td className="w-25">
+          <div className="d-flex justify-content-center">
+            <Button variant="outline-success mx-1">
+              <Link
+                to="/productpage/edicionproducto"
+                variant="outline-primary mx-1"
+              >
+                <box-icon
+                  type="solid"
+                  name="message-square-edit"
+                ></box-icon>
+              </Link>
+            </Button>
+            <Button variant="outline-danger mx-1">
+              <box-icon
+                name="message-square-x"
+                type="solid"
+              ></box-icon>
+            </Button>
+          </div>
+        </td>
+      </tr>
+      ) )
+      : <Spinner color="warning"/> }
+
+               
               </tbody>
             </Table>
           </Col>
