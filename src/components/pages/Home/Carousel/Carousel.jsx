ba06564c40fc66625f334e9aabdf6d6d182c2402
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2022-12-15 11:55:53 -0300

    se elimino codigo comentado

--- src/components/pages/Home/Carousel/Carousel.jsx
+++ src/components/pages/Home/Carousel/Carousel.jsx
@@ -9,55 +9,15 @@ import cerveza3 from '../../../../assets/img/carousel/cerveza3.jpg'
 
 const Calesita = () => {
   return (
-    // <div>
-    //   <Carousel fade>
-    //     <Carousel.Item interval={1000}>
-    //       <img
-    //         className="d-block w-100 img-carousel"
-    //         src="https://images.everydayhealth.com/images/why-you-could-be-drinking-more-than-you-think-1440x810.jpg"
-    //         alt="cafe"
-    //       />
-    //       <Carousel.Caption>
-    //         <h3>First slide label</h3>
-    //         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    //       </Carousel.Caption>
-    //     </Carousel.Item>
-    //     <Carousel.Item interval={500}>
-    //       <img
-    //         className="d-block w-100 img-carousel"
-    //         src="https://i0.wp.com/www.charlestoncvb.com/blog/wp-content/uploads/EdmundsOast9.jpg?resize=1400%2C750&ssl=1"
-    //         alt="croissant"
-    //       />
-    //       <Carousel.Caption>
-    //         <h3>Second slide label</h3>
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    //       </Carousel.Caption>
-    //     </Carousel.Item>
-    //     <Carousel.Item>
-    //       <img
-    //         className="d-block w-100 img-carousel"
-    //         src="https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=1"
-    //         alt="mixed roasts"
-    //       />
-    //       <Carousel.Caption>
-    //         <h3>Third slide label</h3>
-    //         <p>
-    //           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-    //         </p>
-    //       </Carousel.Caption>
-    //     </Carousel.Item>
-    //   </Carousel>
-    // </div>
-
-    <Carousel>
+    <Carousel fade>
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100"
           src={cerveza1}
-          alt="First slide"
+          alt="cerveza1"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>Cerveza 1</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -65,10 +25,10 @@ const Calesita = () => {
         <img
           className="d-block w-100"
           src={cerveza2}
-          alt="Second slide"
+          alt="cerveza2"
         />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3>Cerveza 2</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -76,10 +36,10 @@ const Calesita = () => {
         <img
           className="d-block w-100"
           src={cerveza3}
-          alt="Third slide"
+          alt="cerveza3"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Cerveza 3</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
